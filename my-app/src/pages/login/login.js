d888ecc7794f5ba770f7a8befb10676985767270
thayllachristine <thayllachristineo@gmail.com>
2018-11-13 14:42:15 -0200

    Novas funções adicionadas no Input

--- my-app/src/pages/login/login.js
+++ my-app/src/pages/login/login.js
@@ -12,9 +12,9 @@ function Login() {
                 text='Entre com seu e-mail e senha'>
 
                 <Form.Label htmlFor='emailInput'>E-mail</Form.Label>
-                <Form.Input type='text' id='emailInput' className='email-input' placeholder='email@email.com' />
+                <Form.Input type='text' id='emailInput' className='email-input' placeholder='email@email.com' required />
                 <Form.Label htmlFor='passwordInput'>Senha</Form.Label>
-                <Form.Input type='password' id='passwordInput' className='password-input' placeholder='******' />
+                <Form.Input type='password' id='passwordInput' className='password-input' placeholder='******' required />
                 <Form.Button disabled>Enviar</Form.Button>
                 <Form.Link href='#'>Criar uma conta</Form.Link>
             </Form>
