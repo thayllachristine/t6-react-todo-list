3aacf09a5bb7a20e15ab6f117d184fb4950bf5b6
thayllachristine <thayllachristineo@gmail.com>
2018-11-13 11:11:46 -0200

    Inserção do Container

--- my-app/src/pages/login/login.js
+++ my-app/src/pages/login/login.js
@@ -1,21 +1,24 @@
 import React from 'react'
 
 import Form from '../../components/form'
+import Container from '../../components/container';
 
 
 function Login() {
     return (
-        <Form
-            title='Login'
-            text='Entre com seu e-mail e senha'>
+        <Container>
+            <Form
+                title='Login'
+                text='Entre com seu e-mail e senha'>
 
-            <Form.Label htmlFor='emailInput'>E-mail</Form.Label>
-            <Form.Input type='text' id='emailInput' className='email-input' placeholder='email@email.com' />
-            <Form.Label htmlFor='passwordInput'>Senha</Form.Label>
-            <Form.Input type='password' id='passwordInput' className='password-input' placeholder='******' />
-            <Form.Button disabled>Enviar</Form.Button>
-            <Form.Link href='#'>Criar uma conta</Form.Link>
-        </Form>
+                <Form.Label htmlFor='emailInput'>E-mail</Form.Label>
+                <Form.Input type='text' id='emailInput' className='email-input' placeholder='email@email.com' />
+                <Form.Label htmlFor='passwordInput'>Senha</Form.Label>
+                <Form.Input type='password' id='passwordInput' className='password-input' placeholder='******' />
+                <Form.Button disabled>Enviar</Form.Button>
+                <Form.Link href='#'>Criar uma conta</Form.Link>
+            </Form>
+        </Container>
     )
 }
 
