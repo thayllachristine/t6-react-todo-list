9b5da83723bd01d4c83f284b08c01d348048d843
thayllachristine <thayllachristineo@gmail.com>
2018-11-13 14:42:47 -0200

    mend

--- my-app/src/components/form/input/input.js
+++ my-app/src/components/form/input/input.js
@@ -1,15 +1,53 @@
 import './input.css'
-import React from 'react'
 
-function Input(props) {
-    return (
-        <input
-            type={props.type}
-            id={props.id}
-            className='input'
-            placeholder={props.placeholder}
-        />
-    )
+import React, { Component } from 'react'
+
+
+class Input extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { message: false }
+        // this.handleChange = this.handleChange.bind(this)
+    }
+
+
+    handleChange = (e) => {
+        const value = e.target.value
+        let message = ``
+
+        console.log('oi', value)
+
+        if (this.props.required && value.trim() === ``) {
+            message = `Campo obrigatório`
+        }
+
+        this.setState({ message: message })
+    }
+
+
+    render() {
+        return (
+            <React.Fragment>
+                <input
+                    onChange={this.handleChange}
+                    onBlur={this.handleChange}
+
+                    type={this.props.type}
+                    id={this.props.id}
+                    className='input'
+                    placeholder={this.props.placeholder}
+
+                    required={this.props.required}
+                />
+
+                <p className='input__erro'>{this.state.message}</p>
+
+            </React.Fragment>
+        )
+    }
+
 }
+
+
 
 export default Input
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,24 +3,12 @@ import ReactDOM from 'react-dom';
 
 import './index.css'
 
-import Form from './components/form/form'
+import Login from './pages/login'
 
 import * as serviceWorker from './serviceWorker';
 
-const component = <Form
-    title='Login'
-    text='Entre com seu e-mail e senha'>
 
-    <Form.Label htmlFor='emailInput'>E-mail</Form.Label>
-    <Form.Input type='text' id='emailInput' className='email-input' placeholder='email@email.com' />
-    <Form.Label htmlFor='passwordInput'>E-mail</Form.Label>
-    <Form.Input type='password' id='passwordInput' className='password-input' placeholder='******' />
-    <Form.Button disabled>Enviar</Form.Button>
-    <Form.Link href='#'>Criar uma conta</Form.Link>
-</Form>
-
-
-ReactDOM.render(component, document.getElementById('root'));
+ReactDOM.render(< Login />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
