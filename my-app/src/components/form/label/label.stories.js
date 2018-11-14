4dc4462e52a39058677085505fef46d5f22d726c
thayllachristine <thayllachristineo@gmail.com>
2018-11-14 16:06:55 -0200

    label.stories.js

---
+++ my-app/src/components/form/label/label.stories.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+
+import Label from './index'
+
+storiesOf(`Label`, module)
+    .add(`default`, () => (
+        <Label />
+    ))
