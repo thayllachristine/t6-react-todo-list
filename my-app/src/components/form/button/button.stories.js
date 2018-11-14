b40b367cd2381260eb1871ded7c208ec1415b350
thayllachristine <thayllachristineo@gmail.com>
2018-11-14 16:41:59 -0200

    add input.stories.js

---
+++ my-app/src/components/form/button/button.stories.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+
+import Button from './index'
+
+storiesOf(`Components/Form/Button`, module)
+    .add(`default`, () => (
+        <Button></Button>
+    ))
