4c18a8384d3a4f1e6383b267ba8f9b36e5636af0
thayllachristine <thayllachristineo@gmail.com>
2018-11-14 16:06:31 -0200

    input.stories.js

---
+++ my-app/src/components/form/input/input.stories.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+
+import Input from './index'
+
+storiesOf(`Input`, module)
+    .add(`default`, () => (
+        <Input type='text' />
+    ))
